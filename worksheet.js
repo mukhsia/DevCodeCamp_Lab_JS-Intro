f58ecad646aec34d1a2779e22656432089ff5134
mukhsia <mukhsia@pdx.edu>
2023-11-14 17:20:35 -0800

    Completed Conditinals Task 1.

--- worksheet.js
+++ worksheet.js
@@ -1,3 +1,4 @@
+// Variables
 // Task 1: Variables
 /*
 let dayOfWeek = `Monday`;
@@ -14,3 +15,21 @@ let colorInput = prompt(`What is your favorite color?`);
 console.log(`I've never seen a ${colorInput} ${animalInput}!`);
 */
 
+// Conditionals
+// Fav Breakfast: Protein Shake
+// Fav Lunch: Sandwich
+// Fav Dinner: Steak
+
+// Task 1: Meals
+/*
+let timeOfDay = 1300;
+let meal;
+if (timeOfDay < 1200) {
+    meal = `Protein Shake`;
+} else if (timeOfDay >= 1200 && timeOfDay < 1700) {
+    meal = `Sandwich`;
+} else if (timeOfDay >= 1700) {
+    meal = `Steak`;
+}
+console.log(meal);
+*/
