d18afdf0b894d09d9bde5502322b0ec5681debbd
mukhsia <mukhsia@pdx.edu>
2023-11-14 17:10:20 -0800

    Completed Task 2.

--- worksheet.js
+++ worksheet.js
@@ -1,6 +1,16 @@
 // Task 1: Variables
+/*
 let dayOfWeek = `Monday`;
 console.log(dayOfWeek);
 dayOfWeek = `Friday`;
 console.log(`I can't wait for ${dayOfWeek}`);
+*/
+
+
+// Task 2: User Input
+/*
+let animalInput = prompt(`What is your favorite animal?`);
+let colorInput = prompt(`What is your favorite color?`);
+console.log(`I've never seen a ${colorInput} ${animalInput}!`);
+*/
 
